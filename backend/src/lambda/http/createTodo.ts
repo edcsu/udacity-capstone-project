327ecfc75167fccd9c10964a997beeb7324f0498
Keith Ssewanonda <38857594+edcsu@users.noreply.github.com>
2021-09-09 02:34:54 +0300

    initial commit

---
+++ backend/src/lambda/http/createTodo.ts
@@ -0,0 +1,42 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import 'source-map-support/register'
+import * as middy from 'middy'
+import { cors } from 'middy/middlewares'
+import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import { getUserId } from '../utils';
+import { createTodo } from '../../helpers/todos'
+import { createLogger } from '../../utils/logger'
+
+const logger = createLogger('createTodo')
+
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    
+    logger.info('Processing createTodo event', { event })
+      
+    // TODO: Implement creating a new TODO item
+    const userId = getUserId(event)
+    const newTodo: CreateTodoRequest = JSON.parse(event.body)
+
+    const newItem = await createTodo(userId, newTodo)
+
+    logger.info(`A new todo created with name: ${newTodo.name}`)
+    
+    return {
+      statusCode: 201,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        item: newItem
+      })
+    }
+  }
+)
+
+handler.use(
+  cors({
+    credentials: true
+  })
+)
---
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -0,0 +1,44 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import * as middy from 'middy'
+import { cors, httpErrorHandler } from 'middy/middlewares'
+
+import { generateUploadUrl } from '../../helpers/todos'
+
+import { createLogger } from '../../utils/logger'
+
+
+const logger = createLogger('generateUploadUrl')
+
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    
+    logger.info('Processing generateUploadUrl event', { event })
+    
+    // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
+    
+    const attachmentId = event.pathParameters.todoId
+
+    const uploadUrl = await generateUploadUrl(attachmentId)
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        uploadUrl
+      })
+    }
+  }
+)
+
+handler
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
+    })
+  )
